8f399d260a77a0f8cdb2c540b2af08f2abbde24b
WZY <wzy@qq.com>
2018-10-30 09:20:31 +0800

    咨询修改bug

--- app/static/js/Hisconsult.js
+++ app/static/js/Hisconsult.js
@@ -1,21 +1,21 @@
 $(function(){
-    $("#sub").click(function(){
-        if(!$("#ta").val()){
-            alert("回复内容没填呢!")
-        }
-        else if(window.confirm("确认提交?")){
-            $.ajax({
-                type: "POST",
-                url: "/Hisreply",
-                data: {reply:$("#ta").val(), id:$("#input").val()},
-                dataType: "text",
-                success: function(data){
-                    alert(data);
-                    location.href = "/Hisconsult"
-                },
-            });
-        }else{
-            return 
-        }
-    });
+        $(".sub").click(function(){
+            var text = $(this).parent().prev().children().val()
+            var id = $(this).parent().parent().find('input[type=hidden]').val()
+            if(!text){
+                alert("回复内容没填呢!")
+            }
+            else if(window.confirm("确认提交?")){
+                $.ajax({
+                    type: "POST",
+                    url: "/Hisreply",
+                    data: {reply:text, id:id},
+                    dataType: "text",
+                    success: function(data){
+                        alert(data);
+                        location.href = "/Hisconsult"
+                    },
+                });
+            }
+        });
 });
