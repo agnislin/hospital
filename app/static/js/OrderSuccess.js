1e9aa7df63d04dc799af5e06756ae9991bccd0c5
linyi <1115048631@qq.com>
2018-11-01 17:49:41 +0800

    update all

--- app/static/js/OrderSuccess.js
+++
@@ -1,7 +0,0 @@
-function get_weekday(date) {
-    var date = new Date(date+' 0:0:0');
-    var weeks = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
-    var day = weeks[date.getDay()];
-    var date = day+$('#div_time').text();
-    $('#div_time').text(date)   
-}
--- manage.py
+++
@@ -1,9 +0,0 @@
-# 启动和管理项目
-from app import create_app
-
-app = create_app()
-ctx = app.app_context()
-ctx.push()
-
-if __name__ == '__main__':
-    app.run()
